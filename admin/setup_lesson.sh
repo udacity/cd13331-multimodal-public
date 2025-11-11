7e6fbd593dd4527b516734c3c1a5c1b889989332
Giacomo Vianello <giacomo.vianello@gmail.com>
2025-11-11 15:42:13 -0800

    Setup script

---
+++ admin/setup_lesson.sh
@@ -0,0 +1,23 @@
+# Setup the environment for a lesson
+# Usage:
+# > bash setup_lesson.sh [lesson]
+# Example:
+# > bash setup_lesson.sh l1
+
+lesson="${1}"
+
+export UV_CACHE_DIR=/workspace/.cache/uv
+
+mkdir -p $UV_CACHE_DIR
+
+mkdir -p /workspace/.cache/wheels
+
+if [ "$lesson" == "l1" ]; then
+	curl -L -O https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.4.11/flash_attn-2.8.3+cu124torch2.8-cp312-cp312-linux_x86_64.whl
+	mv flash_attn-2.8.3+cu124torch2.8-cp312-cp312-linux_x86_64.whl /workspace/.cache/wheels
+fi
+
+cd $lesson
+export UV_PROJECT_ENVIRONMENT=/workspace/.venv
+
+uv sync
