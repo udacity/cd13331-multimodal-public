5382ab91417b555773acdbad2fbbb1cfa13dfbe6
Giacomo Vianello <giacomo@capeanalytics.com>
2025-11-12 06:25:28 +0000

    Moving things in the right place

--- admin/setup_lesson.sh
+++ admin/setup_lesson.sh
@@ -6,11 +6,13 @@
 
 lesson="${1}"
 
-export UV_CACHE_DIR=/workspace/.cache/uv
+export DATA_ROOT=/mnt/test-data
+
+export UV_CACHE_DIR=${DATA_ROOT}/.cache/uv
 
 mkdir -p $UV_CACHE_DIR
 
-mkdir -p /workspace/.cache/wheels
+mkdir -p ${DATA_ROOT}/.cache/wheels
 
 if [ "$lesson" == "l1" ]; then
 	curl -L -O https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.4.11/flash_attn-2.8.3+cu124torch2.8-cp312-cp312-linux_x86_64.whl
